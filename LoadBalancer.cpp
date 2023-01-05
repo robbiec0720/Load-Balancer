af5ced50900db266711b2f17411710109dae3dc0
robbiec0720 <robbiec0720@gmail.com>
2023-01-05 09:51:21 -0600

    load balancer doxygen

--- LoadBalancer.cpp
+++ LoadBalancer.cpp
@@ -2,43 +2,76 @@
 
 using namespace std;
 
+/**
+ * @brief Returns true if the request queue is empty, false otherwise.
+ *
+ * @return True if the request queue is empty, false otherwise.
+ */
 bool LoadBalancer::is_empty() {
     return request_queue.empty();
 }
 
+/**
+ * @brief Assigns a request from the request queue to a server.
+ *
+ * If no server is available, the request is discarded.
+ *
+ * @param t The current time.
+ */
 void LoadBalancer::assign_request(int t) {
     bool full = true;
     for(int i = 0; i < server_list.size(); i++) {
         if(server_list[i].poll(t) && !is_empty()) {
             server_list[i].process_request(request_queue.front(), t);
-            //cout << request_queue.front().get_time() << endl;
             request_queue.pop();
             full = false;
-            //cout << "Cycle " << t << ": Assigned request to webserver " << (i+1) << "." << endl;
         }
     }
     if(full) {
-        //cout << "Cycle " << t << ": All webservers are currently full." << endl;
+        // Discard request
     }
 }
 
+/**
+ * @brief Handles a request by adding it to the request queue.
+ *
+ * If the request queue is full, the request is discarded.
+ *
+ * @param r The request to handle.
+ * @param t The current time.
+ */
 void LoadBalancer::handle_request(Request r, int t) {
     if(request_queue.size() == capacity) {
-        //cout << "Cycle " << t << ": Process Queue is full, discarding request." << endl;
+        // Discard request
     }
     else {
         request_queue.push(r);
     }
 }
 
+/**
+ * @brief Adds a web server to the load balancer.
+ *
+ * @param ws The web server to add.
+ */
 void LoadBalancer::add_web_server(WebServer ws) {
     server_list.push_back(ws);
 }
 
+/**
+ * @brief Constructs a new LoadBalancer with the given capacity.
+ *
+ * @param desired_capacity The maximum number of servers the load balancer can manage.
+ */
 LoadBalancer::LoadBalancer(int desired_capacity) {
     capacity = desired_capacity;
 }
 
+/**
+ * @brief Returns the number of servers managed by the load balancer.
+ *
+ * @return The number of servers managed by the load balancer.
+ */
 int LoadBalancer::get_size() {
     return request_queue.size();
 }
--- LoadBalancer.h
+++ LoadBalancer.h
@@ -8,16 +8,70 @@
 
 using namespace std;
 
+/**
+ * @brief A class representing a load balancer.
+ *
+ * This class stores a list of web servers and a queue of requests, and provides
+ * methods for adding servers, handling requests, and assigning requests to servers.
+ */
 class LoadBalancer {
 private:
+    /**
+     * @var capacity The maximum number of servers the load balancer can manage.
+     */
     int capacity;
+
+    /**
+     * @var request_queue The queue of requests waiting to be processed.
+     */
     queue<Request> request_queue;
+
+    /**
+     * @var server_list The list of servers managed by the load balancer.
+     */
     vector<WebServer> server_list;
+
 public:
+    /**
+     * @brief Constructs a new LoadBalancer with the given capacity.
+     *
+     * @param desiredCapacity The maximum number of servers the load balancer can manage.
+     */
     LoadBalancer(int desiredCapacity);
+
+    /**
+     * @brief Adds a web server to the load balancer.
+     *
+     * @param ws The web server to add.
+     */
     void add_web_server(WebServer ws);
+
+    /**
+     * @brief Returns true if the request queue is empty, false otherwise.
+     *
+     * @return True if the request queue is empty, false otherwise.
+     */
     bool is_empty();
+
+    /**
+     * @brief Handles a request by adding it to the request queue.
+     *
+     * @param r The request to handle.
+     * @param t The current time.
+     */
     void handle_request(Request r, int t);
+
+    /**
+     * @brief Assigns a request from the request queue to a server.
+     *
+     * @param t The current time.
+     */
     void assign_request(int t);
+
+    /**
+     * @brief Returns the number of servers managed by the load balancer.
+     *
+     * @return The number of servers managed by the load balancer.
+     */
     int get_size();
 };
