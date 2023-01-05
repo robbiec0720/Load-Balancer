04c526b89fe74582ae658e33c56cbb572e251442
robbiec0720 <robbiec0720@gmail.com>
2023-01-05 09:53:53 -0600

    request doxygen comments

--- Request.cpp
+++ Request.cpp
@@ -1,19 +1,39 @@
 #include "Request.h"
 
+/**
+ * @brief Constructs a new Request.
+ *
+ * The processing time, source address, and destination address are initialized to random values.
+ */
 Request::Request() {
     process_time = rand() % 499 + 2;
     in = IPAddress();
     out = IPAddress();
 }
 
+/**
+ * @brief Returns the processing time of the request.
+ *
+ * @return The processing time of the request.
+ */
 int Request::get_time() {
     return process_time;
 }
 
+/**
+ * @brief Returns a reference to the source address of the request.
+ *
+ * @return A reference to the source address of the request.
+ */
 IPAddress& Request::get_in() {
     return in;
 }
 
+/**
+ * @brief Returns a reference to the destination address of the request.
+ *
+ * @return A reference to the destination address of the request.
+ */
 IPAddress& Request::get_out() {
     return out;
 }
--- Request.h
+++ Request.h
@@ -2,14 +2,54 @@
 
 #include "IPAddress.h"
 
+/**
+ * @brief A class representing a request.
+ *
+ * This class stores the processing time and source and destination addresses of a request.
+ */
 class Request {
 private:
+    /**
+     * @var process_time The processing time of the request.
+     */
     int process_time;
+
+    /**
+     * @var in The source address of the request.
+     */
     IPAddress in;
+
+    /**
+     * @var out The destination address of the request.
+     */
     IPAddress out;
+
 public:
+    /**
+     * @brief Constructs a new Request.
+     *
+     * The processing time, source address, and destination address are initialized to random values.
+     */
     Request();
+
+    /**
+     * @brief Returns the processing time of the request.
+     *
+     * @return The processing time of the request.
+     */
     int get_time();
+
+    /**
+     * @brief Returns a reference to the source address of the request.
+     *
+     * @return A reference to the source address of the request.
+     */
     IPAddress& get_in();
+
+    /**
+     * @brief Returns a reference to the destination address of the request.
+     *
+     * @return A reference to the destination address of the request.
+     */
     IPAddress& get_out();
 };
